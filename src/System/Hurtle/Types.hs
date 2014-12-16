bens/hurtle

commit 6fda83c2bb3c73d6d00d02ac2770459737b2c6d7

Get rid of dependency on free package, just use a custom free monad

src/System/Hurtle/Types.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE Safe                       #-}
@@ -7,9 +6,8 @@
 module System.Hurtle.Types where
 
 import           Control.Applicative
+import           Control.Monad             ((>=>), ap)
 import qualified Control.Monad.Par.Class   as Par
-import           Control.Monad.Trans.Free  (Free)
-import qualified Control.Monad.Trans.Free  as Free
 
 import           System.Hurtle.Common
 import qualified System.Hurtle.TypedStore  as TS
@@ -30,7 +28,7 @@ instance OrdF (SentRequest s t c) where
     SR i _ `compareF` SR j _ = BlindOrdF i `compare` BlindOrdF j
 
 data BlockedProcess s c a where
-    BP :: ForkId s c b -> (a -> Free (HurtleF s c) b) -> BlockedProcess s c a
+    BP :: ForkId s c b -> (a -> Hurtle s c b) -> BlockedProcess s c a
 
 data SomeProcess s c a
     = ProcessDone a
@@ -38,7 +36,7 @@ data SomeProcess s c a
     | ProcessRunning [BlockedProcess s c a]
 
 data SomeRequest s c b a =
-    Req (Request c a) (a -> Free (HurtleF s c) b)
+    Req (Request c a) (a -> Hurtle s c b)
 
 data HurtleState s t c = HState
     { _stNextId   :: Int
@@ -47,26 +45,28 @@ data HurtleState s t c = HState
     , _stInFlight :: TS2.TypedStore t TS2.NonMono (SomeRequest s c)
     }
 
-data HurtleF s c a where
-    CallF  :: Request c b -> (b -> a) -> HurtleF s c a
-    ForkF  :: Free (HurtleF s c) b -> (ForkId s c b -> a) -> HurtleF s c a
-    BlockF :: ForkId s c b -> (b -> a) -> HurtleF s c a
+data Hurtle s c a where
+    Done :: a -> Hurtle s c a
+    Call :: Request c b -> (b -> Hurtle s c a) -> Hurtle s c a
+    Fork :: Hurtle s c b -> (ForkId s c b -> Hurtle s c a) -> Hurtle s c a
+    Block :: ForkId s c b -> (b -> Hurtle s c a) -> Hurtle s c a
 
-instance Functor (HurtleF s c) where
-    fmap f (CallF req k)  = CallF req (f . k)
-    fmap f (ForkF m k)    = ForkF m (f . k)
-    fmap f (BlockF fid k) = BlockF fid (f . k)
-
-newtype Hurtle s c a = Hurtle{ unHurtle :: Free (HurtleF s c) a }
-    deriving Functor
+instance Functor (Hurtle s c) where
+    fmap f (Done x)      = Done (f x)
+    fmap f (Call req k)  = Call req  (fmap f . k)
+    fmap f (Fork m k)    = Fork m    (fmap f . k)
+    fmap f (Block fid k) = Block fid (fmap f . k)
 
 instance Applicative (Hurtle s c) where
-    pure = Hurtle . pure
-    Hurtle f <*> Hurtle x = Hurtle $ f <*> x
+    pure = Done
+    f <*> x = ap f x
 
 instance Monad (Hurtle s c) where
-    return = Hurtle . return
-    Hurtle m >>= f = Hurtle $ m >>= unHurtle . f
+    return = Done
+    Done x      >>= f = f x
+    Call req k  >>= f = Call req  (k >=> f)
+    Fork m k    >>= f = Fork m    (k >=> f)
+    Block fid k >>= f = Block fid (k >=> f)
 
 instance Connection c => Par.ParFuture (Hurtle s c) (Hurtle s c) where
     spawn_ = fork
@@ -74,9 +74,8 @@ instance Connection c => Par.ParFuture (Hurtle s c) (Hurtle s c) where
 
 -- | Fork a new process and return an action that will wait for the result.
 fork :: Connection c => Hurtle s c a -> Hurtle s c (Hurtle s c a)
-fork h = Hurtle . Free.liftF . ForkF (unHurtle h) $ \fid ->
-    Hurtle . Free.liftF $ BlockF fid id
+fork h = Fork h $ \fid -> Block fid (return . return)
 
 -- | Make a request and wait for the response.
 request :: Connection c => Request c a -> Hurtle s c a
-request req = Hurtle $ Free.liftF (CallF req id)
+request req = Call req return

hurtle.cabal
@@ -26,7 +26,6 @@ library
       base >=4.6 && <4.8
     , abstract-par
     , containers
-    , free
     , monad-resumption >= 0.1.1.4
     , transformers
   ghc-options:      -Wall

src/System/Hurtle.hs
@@ -72,9 +72,7 @@ import           Control.Monad.Fix                 (fix)
 import           Control.Monad.Resumption          (runResT)
 import           Control.Monad.Resumption.Reactive ((<~>), signal)
 import           Control.Monad.Trans.Class         (lift)
-import           Control.Monad.Trans.Free          (Free, FreeT(..), FreeF(..))
 import           Control.Monad.Trans.State
-import           Data.Functor.Identity
 
 import           System.Hurtle.Common
 import           System.Hurtle.Log
@@ -101,14 +99,14 @@ forkProcess = do
     return (ForkId (_stNextId st) fid)
 
 data Step s c where
-    Step :: ForkId s c a -> Free (HurtleF s c) a -> Step s c
+    Step :: ForkId s c a -> Hurtle s c a -> Step s c
 
 runHurtle :: (Connection c, Applicative (M c), Monad (M c))
           => InitArgs c                                      -- ^ Initialisation
           -> (forall i. Show i => Log (Error c) i -> M c ()) -- ^ Log handler
           -> (forall s. Hurtle s c a)                        -- ^ Action to run
           -> M c (Either (Error c) a)
-runHurtle args logIt' h' = withHurtleState args h' $ \st0 (Hurtle h) -> do
+runHurtle args logIt' h' = withHurtleState args h' $ \st0 h -> do
     let logIt x = logIt' x where _ = [Sending (0 :: Int), x]
 
         unlessDone (ForkId _ fid) k = do
@@ -118,8 +116,8 @@ runHurtle args logIt' h' = withHurtleState args h' $ \st0 (Hurtle h) -> do
                 ProcessFailed err -> return (Left err)
                 ProcessRunning _ -> k
 
-        process (Step forkId@(ForkId cid fid) (FreeT (Identity m))) = case m of
-            Pure x -> do
+        process (Step forkId@(ForkId cid fid) m) = case m of
+            Done x -> do
                 forks <- gets _stForks
                 case fid TS.! forks of
                     ProcessDone _   -> error "wtf?"  -- finished twice?!
@@ -132,17 +130,17 @@ runHurtle args logIt' h' = withHurtleState args h' $ \st0 (Hurtle h) -> do
                         forM_ bps $ \(BP forkId'@(ForkId cid' _) k) -> do
                             lift . logIt $ Continuing cid' cid
                             process (Step forkId' (k x))
-            Free (CallF req k) -> do
+            Call req k -> do
                 lift . logIt $ Sending cid
                 callId <- sendingRequest (Req req k)
                 st <- get
                 lift $ send (_stState st) (SR forkId callId) req
-            Free (ForkF m' k) -> do
+            Fork m' k -> do
                 forkId'@(ForkId cid' _) <- forkProcess
                 lift . logIt $ Forked cid cid'
                 process (Step forkId' m')
                 process (Step forkId (k forkId'))
-            Free (BlockF (ForkId cid' fid') k) -> do
+            Block (ForkId cid' fid') k -> do
                 lift . logIt $ Blocked cid cid'
                 forks <- gets _stForks
                 case fid' TS.! forks of
@@ -176,9 +174,7 @@ runHurtle args logIt' h' = withHurtleState args h' $ \st0 (Hurtle h) -> do
                     st <- lift get
                     case TS2.lookup callId (_stInFlight st) of
                         Nothing -> error "wtf?"
-                        Just (Req req k') -> do
-                            let k = FreeT (Identity (Free (CallF req k')))
-                            signal (Step forkId k)
+                        Just (Req req k') -> signal (Step forkId (Call req k'))
                 Fatal Nothing err -> do
                     lift . lift . logIt $ SystemError err
                     st <- lift get

commit 1f8e323636a22372ce91423c6322d3b78982c68c

Fix a regression in 6fda83c2bb3c73d6d00d02ac2770459737b2c6d7

src/System/Hurtle/Types.hs
@@ -69,7 +69,7 @@ instance Par.ParFuture (Hurtle s c) (Hurtle s c) where
 
 -- | Fork a new process and return an action that will wait for the result.
 fork :: Hurtle s c a -> Hurtle s c (Hurtle s c a)
-fork h = Fork h $ \fid -> Block fid (return . return)
+fork h = Fork h $ \fid -> return $ Block fid return
 
 -- | Make a request and wait for the response.
 request :: Request c a -> Hurtle s c a