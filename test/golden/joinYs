bens/hurtle

commit 6fda83c2bb3c73d6d00d02ac2770459737b2c6d7

Get rid of dependency on free package, just use a custom free monad

hurtle.cabal
@@ -26,7 +26,6 @@ library
       base >=4.6 && <4.8
     , abstract-par
     , containers
-    , free
     , monad-resumption >= 0.1.1.4
     , transformers
   ghc-options:      -Wall

src/System/Hurtle.hs
@@ -72,9 +72,7 @@ import           Control.Monad.Fix                 (fix)
 import           Control.Monad.Resumption          (runResT)
 import           Control.Monad.Resumption.Reactive ((<~>), signal)
 import           Control.Monad.Trans.Class         (lift)
-import           Control.Monad.Trans.Free          (Free, FreeT(..), FreeF(..))
 import           Control.Monad.Trans.State
-import           Data.Functor.Identity
 
 import           System.Hurtle.Common
 import           System.Hurtle.Log
@@ -101,14 +99,14 @@ forkProcess = do
     return (ForkId (_stNextId st) fid)
 
 data Step s c where
-    Step :: ForkId s c a -> Free (HurtleF s c) a -> Step s c
+    Step :: ForkId s c a -> Hurtle s c a -> Step s c
 
 runHurtle :: (Connection c, Applicative (M c), Monad (M c))
           => InitArgs c                                      -- ^ Initialisation
           -> (forall i. Show i => Log (Error c) i -> M c ()) -- ^ Log handler
           -> (forall s. Hurtle s c a)                        -- ^ Action to run
           -> M c (Either (Error c) a)
-runHurtle args logIt' h' = withHurtleState args h' $ \st0 (Hurtle h) -> do
+runHurtle args logIt' h' = withHurtleState args h' $ \st0 h -> do
     let logIt x = logIt' x where _ = [Sending (0 :: Int), x]
 
         unlessDone (ForkId _ fid) k = do
@@ -118,8 +116,8 @@ runHurtle args logIt' h' = withHurtleState args h' $ \st0 (Hurtle h) -> do
                 ProcessFailed err -> return (Left err)
                 ProcessRunning _ -> k
 
-        process (Step forkId@(ForkId cid fid) (FreeT (Identity m))) = case m of
-            Pure x -> do
+        process (Step forkId@(ForkId cid fid) m) = case m of
+            Done x -> do
                 forks <- gets _stForks
                 case fid TS.! forks of
                     ProcessDone _   -> error "wtf?"  -- finished twice?!
@@ -132,17 +130,17 @@ runHurtle args logIt' h' = withHurtleState args h' $ \st0 (Hurtle h) -> do
                         forM_ bps $ \(BP forkId'@(ForkId cid' _) k) -> do
                             lift . logIt $ Continuing cid' cid
                             process (Step forkId' (k x))
-            Free (CallF req k) -> do
+            Call req k -> do
                 lift . logIt $ Sending cid
                 callId <- sendingRequest (Req req k)
                 st <- get
                 lift $ send (_stState st) (SR forkId callId) req
-            Free (ForkF m' k) -> do
+            Fork m' k -> do
                 forkId'@(ForkId cid' _) <- forkProcess
                 lift . logIt $ Forked cid cid'
                 process (Step forkId' m')
                 process (Step forkId (k forkId'))
-            Free (BlockF (ForkId cid' fid') k) -> do
+            Block (ForkId cid' fid') k -> do
                 lift . logIt $ Blocked cid cid'
                 forks <- gets _stForks
                 case fid' TS.! forks of
@@ -176,9 +174,7 @@ runHurtle args logIt' h' = withHurtleState args h' $ \st0 (Hurtle h) -> do
                     st <- lift get
                     case TS2.lookup callId (_stInFlight st) of
                         Nothing -> error "wtf?"
-                        Just (Req req k') -> do
-                            let k = FreeT (Identity (Free (CallF req k')))
-                            signal (Step forkId k)
+                        Just (Req req k') -> signal (Step forkId (Call req k'))
                 Fatal Nothing err -> do
                     lift . lift . logIt $ SystemError err
                     st <- lift get

src/System/Hurtle/Types.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE Safe                       #-}
@@ -7,9 +6,8 @@
 module System.Hurtle.Types where
 
 import           Control.Applicative
+import           Control.Monad             ((>=>), ap)
 import qualified Control.Monad.Par.Class   as Par
-import           Control.Monad.Trans.Free  (Free)
-import qualified Control.Monad.Trans.Free  as Free
 
 import           System.Hurtle.Common
 import qualified System.Hurtle.TypedStore  as TS
@@ -30,7 +28,7 @@ instance OrdF (SentRequest s t c) where
     SR i _ `compareF` SR j _ = BlindOrdF i `compare` BlindOrdF j
 
 data BlockedProcess s c a where
-    BP :: ForkId s c b -> (a -> Free (HurtleF s c) b) -> BlockedProcess s c a
+    BP :: ForkId s c b -> (a -> Hurtle s c b) -> BlockedProcess s c a
 
 data SomeProcess s c a
     = ProcessDone a
@@ -38,7 +36,7 @@ data SomeProcess s c a
     | ProcessRunning [BlockedProcess s c a]
 
 data SomeRequest s c b a =
-    Req (Request c a) (a -> Free (HurtleF s c) b)
+    Req (Request c a) (a -> Hurtle s c b)
 
 data HurtleState s t c = HState
     { _stNextId   :: Int
@@ -47,26 +45,28 @@ data HurtleState s t c = HState
     , _stInFlight :: TS2.TypedStore t TS2.NonMono (SomeRequest s c)
     }
 
-data HurtleF s c a where
-    CallF  :: Request c b -> (b -> a) -> HurtleF s c a
-    ForkF  :: Free (HurtleF s c) b -> (ForkId s c b -> a) -> HurtleF s c a
-    BlockF :: ForkId s c b -> (b -> a) -> HurtleF s c a
+data Hurtle s c a where
+    Done :: a -> Hurtle s c a
+    Call :: Request c b -> (b -> Hurtle s c a) -> Hurtle s c a
+    Fork :: Hurtle s c b -> (ForkId s c b -> Hurtle s c a) -> Hurtle s c a
+    Block :: ForkId s c b -> (b -> Hurtle s c a) -> Hurtle s c a
 
-instance Functor (HurtleF s c) where
-    fmap f (CallF req k)  = CallF req (f . k)
-    fmap f (ForkF m k)    = ForkF m (f . k)
-    fmap f (BlockF fid k) = BlockF fid (f . k)
-
-newtype Hurtle s c a = Hurtle{ unHurtle :: Free (HurtleF s c) a }
-    deriving Functor
+instance Functor (Hurtle s c) where
+    fmap f (Done x)      = Done (f x)
+    fmap f (Call req k)  = Call req  (fmap f . k)
+    fmap f (Fork m k)    = Fork m    (fmap f . k)
+    fmap f (Block fid k) = Block fid (fmap f . k)
 
 instance Applicative (Hurtle s c) where
-    pure = Hurtle . pure
-    Hurtle f <*> Hurtle x = Hurtle $ f <*> x
+    pure = Done
+    f <*> x = ap f x
 
 instance Monad (Hurtle s c) where
-    return = Hurtle . return
-    Hurtle m >>= f = Hurtle $ m >>= unHurtle . f
+    return = Done
+    Done x      >>= f = f x
+    Call req k  >>= f = Call req  (k >=> f)
+    Fork m k    >>= f = Fork m    (k >=> f)
+    Block fid k >>= f = Block fid (k >=> f)
 
 instance Connection c => Par.ParFuture (Hurtle s c) (Hurtle s c) where
     spawn_ = fork
@@ -74,9 +74,8 @@ instance Connection c => Par.ParFuture (Hurtle s c) (Hurtle s c) where
 
 -- | Fork a new process and return an action that will wait for the result.
 fork :: Connection c => Hurtle s c a -> Hurtle s c (Hurtle s c a)
-fork h = Hurtle . Free.liftF . ForkF (unHurtle h) $ \fid ->
-    Hurtle . Free.liftF $ BlockF fid id
+fork h = Fork h $ \fid -> Block fid (return . return)
 
 -- | Make a request and wait for the response.
 request :: Connection c => Request c a -> Hurtle s c a
-request req = Hurtle $ Free.liftF (CallF req id)
+request req = Call req return

commit 046c393023330b8f9ea025d9e2a7d2257e5e937c

Golden tests
Rather than just eyeballing debug output, keep track of old output and check
against the current tests.  The tests right now don't pass because of a
regression in 6fda83c2bb3c73d6d00d02ac2770459737b2c6d7.

hurtle.cabal
@@ -38,9 +38,13 @@ test-suite hurtle-test
   build-depends:
       base >=4.6 && <4.8
     , containers
+    , directory
+    , filepath
     , hslogger
     , hurtle
     , stm
+    , tasty
+    , tasty-golden
   ghc-options:      -Wall
   hs-source-dirs:   test-src
   default-language: Haskell2010

test-src/Main.hs
@@ -1,16 +1,23 @@
 {-# LANGUAGE GADTs        #-}
+{-# LANGUAGE RankNTypes   #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Main where
 
 import           Control.Applicative
 import qualified Control.Concurrent.STM    as STM
+import           Control.Exception         (finally)
 import           Control.Monad             (join)
 import           Data.List                 (sortBy)
 import qualified Data.Map                  as M
-import           System.IO                 (stderr)
-import qualified System.Log.Handler.Simple as Log
+import           System.Directory          (createDirectoryIfMissing)
+import           System.FilePath           ((</>))
+import           System.IO                 (Handle, IOMode(..), withFile)
+import qualified System.Log.Handler        as LogH
+import qualified System.Log.Handler.Simple as LogH
 import qualified System.Log.Logger         as Log
+import qualified Test.Tasty                as Tasty
+import qualified Test.Tasty.Golden         as Tasty
 
 import           System.Hurtle
 
@@ -59,12 +66,16 @@ instance Connection TestConn where
             Left  (WrapOrdF cid, x) -> return $ Ok cid x
             Right (WrapOrdF cid, x) -> return $ Ok cid (show x)
 
-setupLogging :: IO ()
-setupLogging = do
-    handler <- Log.verboseStreamHandler stderr Log.DEBUG
+withLogging :: FilePath -> IO a -> IO a
+withLogging path m = withFile ("test/output/" </> path) WriteMode $ \h -> do
+    fileH <- LogH.streamHandler h Log.DEBUG
     let setLevel   = Log.setLevel Log.DEBUG
-        setHandler = Log.setHandlers [handler]
+        setHandler = Log.setHandlers [fileH]
+        noHandlers = Log.setHandlers ([] :: [LogH.GenericHandler Handle])
     Log.updateGlobalLogger Log.rootLoggerName (setLevel . setHandler)
+    finally m $ do
+        LogH.close fileH
+        Log.updateGlobalLogger Log.rootLoggerName noHandlers
 
 logHandler :: Show i => Log (Error TestConn) i -> IO ()
 logHandler msg = case logLevel msg of
@@ -77,8 +88,9 @@ logHandler msg = case logLevel msg of
     showE (ErrTest x) = "ERR: " ++ x
 
 main :: IO ()
-main =
-    mainNew
+main = do
+    createDirectoryIfMissing True "test/output"
+    Tasty.defaultMain goldenTests
 
 echoIntNew :: Int -> Hurtle s TestConn Int
 echoIntNew x = request (ReqInt x)
@@ -86,19 +98,59 @@ echoIntNew x = request (ReqInt x)
 showIntNew :: Int -> Hurtle s TestConn String
 showIntNew x = request (ReqStr x)
 
-mainNew :: IO ()
-mainNew = do
-    let test 0 = fork $ pure <$> request (ReqInt 0)
-        test 1 = fork $ pure <$> request (ReqInt 1)
-        test n = do
-            xm <- fork $ request (ReqInt n)
-            xsm <- test (n-1)
-            ysm <- test (n-2)
-            return $ do
-                x  <- xm
-                xs <- xsm
-                ys <- ysm
-                return $ sortBy (flip compare) (x : xs ++ ys)
+type Test s
+    =  Hurtle s TestConn Int
+    -> Hurtle s TestConn [Int]
+    -> Hurtle s TestConn [Int]
+    -> Hurtle s TestConn (Hurtle s TestConn [Int])
 
-    setupLogging
-    runHurtle InitTest logHandler (join (test 5)) >>= print
+goldenTests :: Tasty.TestTree
+goldenTests = Tasty.testGroup "Golden"
+    [ runGolden "forkAll" (forkAll 5)
+    , runGolden "joinXs"  (joinXs  5)
+    , runGolden "joinYs"  (joinYs  5)
+    ]
+
+runGolden :: String -> (forall s. Hurtle s TestConn [Int]) -> Tasty.TestTree
+runGolden nm m =
+    Tasty.goldenVsFile nm golden output . withLogging nm $ do
+        x <- runHurtle InitTest logHandler m
+        Log.infoM "main" $ "RESULT: " ++ show x
+  where
+    golden = "test/golden" </> nm
+    output = "test/output" </> nm
+
+makeGolden :: Test s -> Int -> Hurtle s TestConn [Int]
+makeGolden test = join . go
+  where
+    go 0 = fork $ pure <$> request (ReqInt 0)
+    go 1 = fork $ pure <$> request (ReqInt 1)
+    go n = do
+        xm  <- fork $ request (ReqInt n)
+        xsm <- go (n-1)
+        ysm <- go (n-2)
+        fmap (sortBy (flip compare)) <$> test xm xsm ysm
+
+forkAll :: Int -> Hurtle s TestConn [Int]
+forkAll = makeGolden $ \xm xsm ysm ->
+    return $ do
+        x  <- xm
+        xs <- xsm
+        ys <- ysm
+        return $ x : xs ++ ys
+
+joinXs :: Int -> Hurtle s TestConn [Int]
+joinXs = makeGolden $ \xm xsm ysm -> do
+    xs <- xsm
+    return $ do
+        x  <- xm
+        ys <- ysm
+        return $ x : xs ++ ys
+
+joinYs :: Int -> Hurtle s TestConn [Int]
+joinYs = makeGolden $ \xm xsm ysm -> do
+    ys <- ysm
+    return $ do
+        x  <- xm
+        xs <- xsm
+        return $ x : xs ++ ys

test/golden/forkAll
@@ -1,0 +1,94 @@
+Initialising
+0 forked 1
+1 -->
+0 forked 2
+2 -->
+0 forked 3
+3 -->
+0 forked 4
+4 -->
+0 forked 5
+5 -->
+0 forked 6
+6 -->
+0 forked 7
+7 -->
+0 forked 8
+8 -->
+0 forked 9
+9 -->
+0 forked 10
+10 -->
+0 forked 11
+11 -->
+0 forked 12
+12 -->
+0 forked 13
+13 -->
+0 forked 14
+14 -->
+0 forked 15
+15 -->
+0 waiting for 1
+1 <--
+1 finished
+0 continuing with result from 1
+0 waiting for 2
+2 <--
+2 finished
+0 continuing with result from 2
+0 waiting for 3
+3 <--
+3 finished
+0 continuing with result from 3
+0 waiting for 4
+4 <--
+4 finished
+0 continuing with result from 4
+0 waiting for 5
+5 <--
+5 finished
+0 continuing with result from 5
+0 waiting for 6
+6 <--
+6 finished
+0 continuing with result from 6
+0 waiting for 7
+7 <--
+7 finished
+0 continuing with result from 7
+0 waiting for 8
+8 <--
+8 finished
+0 continuing with result from 8
+0 waiting for 9
+9 <--
+9 finished
+0 continuing with result from 9
+0 waiting for 10
+10 <--
+10 finished
+0 continuing with result from 10
+0 waiting for 11
+11 <--
+11 finished
+0 continuing with result from 11
+0 waiting for 12
+12 <--
+12 finished
+0 continuing with result from 12
+0 waiting for 13
+13 <--
+13 finished
+0 continuing with result from 13
+0 waiting for 14
+14 <--
+14 finished
+0 continuing with result from 14
+0 waiting for 15
+15 <--
+15 finished
+0 continuing with result from 15
+0 finished
+Finished
+RESULT: Right [5,4,3,3,2,2,2,1,1,1,1,1,0,0,0]

test/golden/joinXs
@@ -1,0 +1,94 @@
+Initialising
+0 forked 1
+1 -->
+0 forked 2
+2 -->
+0 forked 3
+3 -->
+0 forked 4
+4 -->
+0 forked 5
+5 -->
+0 forked 6
+6 -->
+0 waiting for 5
+1 <--
+1 finished
+2 <--
+2 finished
+3 <--
+3 finished
+4 <--
+4 finished
+5 <--
+5 finished
+0 continuing with result from 5
+0 forked 7
+7 -->
+0 waiting for 4
+0 continuing with result from 4
+0 waiting for 6
+6 <--
+6 finished
+0 continuing with result from 6
+0 forked 8
+8 -->
+0 forked 9
+9 -->
+0 forked 10
+10 -->
+0 waiting for 9
+7 <--
+7 finished
+8 <--
+8 finished
+9 <--
+9 finished
+0 continuing with result from 9
+0 waiting for 3
+0 continuing with result from 3
+0 waiting for 7
+0 continuing with result from 7
+0 forked 11
+11 -->
+0 forked 12
+12 -->
+0 forked 13
+13 -->
+0 forked 14
+14 -->
+0 waiting for 13
+10 <--
+10 finished
+11 <--
+11 finished
+12 <--
+12 finished
+13 <--
+13 finished
+0 continuing with result from 13
+0 forked 15
+15 -->
+0 waiting for 12
+0 continuing with result from 12
+0 waiting for 14
+14 <--
+14 finished
+0 continuing with result from 14
+0 waiting for 2
+0 continuing with result from 2
+0 waiting for 8
+0 continuing with result from 8
+0 waiting for 10
+0 continuing with result from 10
+0 waiting for 1
+0 continuing with result from 1
+0 waiting for 11
+0 continuing with result from 11
+0 waiting for 15
+15 <--
+15 finished
+0 continuing with result from 15
+0 finished
+Finished
+RESULT: Right [5,4,3,3,2,2,2,1,1,1,1,1,0,0,0]

test/golden/joinYs
@@ -1,0 +1,94 @@
+Initialising
+0 forked 1
+1 -->
+0 forked 2
+2 -->
+0 forked 3
+3 -->
+0 forked 4
+4 -->
+0 forked 5
+5 -->
+0 forked 6
+6 -->
+0 waiting for 6
+1 <--
+1 finished
+2 <--
+2 finished
+3 <--
+3 finished
+4 <--
+4 finished
+5 <--
+5 finished
+6 <--
+6 finished
+0 continuing with result from 6
+0 forked 7
+7 -->
+0 waiting for 7
+7 <--
+7 finished
+0 continuing with result from 7
+0 forked 8
+8 -->
+0 forked 9
+9 -->
+0 forked 10
+10 -->
+0 waiting for 10
+8 <--
+8 finished
+9 <--
+9 finished
+10 <--
+10 finished
+0 continuing with result from 10
+0 waiting for 8
+0 continuing with result from 8
+0 waiting for 9
+0 continuing with result from 9
+0 forked 11
+11 -->
+0 forked 12
+12 -->
+0 forked 13
+13 -->
+0 forked 14
+14 -->
+0 waiting for 14
+11 <--
+11 finished
+12 <--
+12 finished
+13 <--
+13 finished
+14 <--
+14 finished
+0 continuing with result from 14
+0 forked 15
+15 -->
+0 waiting for 15
+15 <--
+15 finished
+0 continuing with result from 15
+0 waiting for 11
+0 continuing with result from 11
+0 waiting for 12
+0 continuing with result from 12
+0 waiting for 13
+0 continuing with result from 13
+0 waiting for 1
+0 continuing with result from 1
+0 waiting for 2
+0 continuing with result from 2
+0 waiting for 3
+0 continuing with result from 3
+0 waiting for 4
+0 continuing with result from 4
+0 waiting for 5
+0 continuing with result from 5
+0 finished
+Finished
+RESULT: Right [5,4,3,3,2,2,2,1,1,1,1,1,0,0,0]